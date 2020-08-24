e272ce4f6cdc1a74cbdf01c2b17d1ef85ac5b6c1
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-08-24 14:38:22 -0300

    Desafio FullCycle Docker

---
+++ DesafioDocker/data.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: "H4CCPaYLTWg",
+        title: "1 Formulario Animado com JS puro e CSS Alvaroico",
+        duration: "12 minutos",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "GGyp_XPhEGo",
+        title: "2 orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, CSS Alvaroico",
+        duration: "30 minutos",
+        price: "Free"
+    },
+    {
+        id: "9F-mKTl1njg",
+        title: "3 Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 200 CSS Alvaroico",
+        duration: "07 minutos",
+        price: "Free"
+    },
+    {
+        id: "A6LP_czEK7U",
+        title: "4 Formulario Animado com JS puro e CSS Alvaroico",
+        duration: "22 minutos",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "Dh5TFPuJXKw",
+        title: "5 There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words whi CSS Alvaroico",
+        duration: "57 minutos",
+        price: "Free"
+    },
+    {
+        id: "iUzxPkr1cWA",
+        title: "6 The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 fromo e CSS Alvaroico",
+        duration: "57 minutos",
+        price: "Free",
+        featured: true
+    }
+]
---
+++ DesafioDocker/server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+const server = express();
+const videos = require("./data");
+
+server.use(express.static("public"));
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+  express: server,
+  autoescape: false,
+  noCache: true,
+});
+
+server.get("/", function (req, res) {
+  const about = {
+    avatar_url: "https://avatars0.githubusercontent.com/u/59286021?v=4",
+    name: "Alvaro Ribeiro Pereira",
+    role: "Eu sou Full Cycle",
+    description:
+      "Durante toda a Maratona utilizaremos containers como base de desenvolvimento bem como para produção. Logo, ter uma noção básica de Docker é mais do que necessário. \
+      Nesse desafio, você terá de criar uma imagem docker que quando executada deverá expor a porta 8080 exibindo a mensagem: Eu sou Full Cycle.\
+      Fique na liberdade para utilizar a tecnologia/linguagem de programação de sua escolha. Exemplo: você poderá criar uma simples aplicação usando Node.js com Express para exibir essa mensagem.\
+      Gere o build da sua imagem, faça o push para o DockerHub e a informe no formulário abaixo.",
+    links: [
+      { name: "GitHub", url: "https://github.com/alvaroico" },
+      { name: "Twitter", url: "https://twitter.com/alvaroico" },
+      { name: "Linkedin", url: "https://www.linkedin.com/in/alvaroico/" },
+    ],
+  };
+
+  return res.render("about", { about: about });
+});
+
+server.get("/portfolio", function (req, res) {
+  return res.render("portfolio", { items: videos });
+});
+
+server.get("/video", function (req, res) {
+  const id = req.query.id;
+
+  const video = videos.find(function (video) {
+    return video.id == id;
+  });
+
+  if (!video) {
+    return res.send("Video not found!");
+  }
+  return res.render("video", { item: video });
+});
+
+server.listen(5000, function () {
+  console.log("Server is Running");
+});
